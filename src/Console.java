feedfb009f7bb07412e1ce3059dcd13826709b92
Yasar Erkan <yasarerkan@Yasars-iMac.local>
2020-11-21 12:22:12 -0500

    added update task name and  update duedate

--- src/Console.java
+++ src/Console.java
@@ -35,7 +35,6 @@ public class Console {
                         "15-To read Tasks from JSON file");
                 int processid = read.nextInt();
                 if (processid == 0) {
-
                     break;
                 } else if (processid == 1 ||processid==2||processid==5||processid==7) {
                     read.nextLine();
@@ -43,16 +42,12 @@ public class Console {
                     int id = Integer.valueOf(read.nextLine());
                     System.out.println("please add task name");
                     String name = read.nextLine();
-
                     System.out.println("please add task due date like this format  (yyyy-MM-dd)");
                     String duedate = read.next();
                     duedate += "T12:00:00";
-
                     //LocalDateTime aLD = LocalDateTime.parse(duedate);
                     //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
                     LocalDateTime dueDate = LocalDateTime.parse(duedate);
-
-
                     switch(processid){
                         case 1:
                             Task.addtask((new Task(id,name,String.valueOf(Task.Status.ADDED),dueDate)));
@@ -62,18 +57,15 @@ public class Console {
                             System.out.println("please add task startdate like this format  (yyyy-MM-dd)");
                             String startdate = read.next();
                             startdate += "T12:00:00";
-
                             LocalDateTime starDate = LocalDateTime.parse(startdate);
                             System.out.println("please add task finishdate like this format  (yyyy-MM-dd)");
                             String finishdate = read.next();
                             finishdate += "T12:00:00";
-
                             LocalDateTime endDate = LocalDateTime.parse(finishdate);
                             Timedtask.addtask((new Timedtask(id, name, String.valueOf(Task.Status.SCHEDULED), dueDate, starDate, endDate)));
                             //listofaddedtimedtasklist.add(new Timedtask(id, name, String.valueOf(Task.Status.SCHEDULED), dueDate, starDate, endDate));
                             break;
                         case 5:
-
                             System.out.println("Who do you want to assign?");
                             String assignedTo = read.next();
                             Assigntask.addtask((new Assigntask(id,name,String.valueOf(Task.Status.ASSIGNED),dueDate,assignedTo)));
@@ -85,11 +77,8 @@ public class Console {
                             Assignedtimedtask.addtask((new Assignedtimedtask(id,name,String.valueOf(Task.Status.SCHEDULEDASSIGNED),dueDate,assignedT,budget)));
                     }
                 }
-
                 else if (processid == 3) {
-
                     System.out.println(Database.listofaddedtasklist);
-
                 }
                 else if (processid == 4) {
                     //ttlist is meaning timed task
@@ -125,162 +114,32 @@ public class Console {
                     System.out.println(sortedTasks);
 
                 }
-
-
-                /*
-                else if (processid == 2) {
-                    System.out.println("which task do you want to timed task  please enter id");
-                    int timedtaskid = read.nextInt();
-
-                    System.out.println("please add task startdate like this format  (yyyy-MM-dd)");
-                    String startdate = read.next();
-                    startdate += "T12:00:00";
-
-                    LocalDateTime starDate = LocalDateTime.parse(startdate);
-                    System.out.println("please add task finishdate like this format  (yyyy-MM-dd)");
-                    String finishdate = read.next();
-                    finishdate += "T12:00:00";
-
-                    LocalDateTime endDate = LocalDateTime.parse(finishdate);
-                    // id that is  was got from user  is  searching  in the list of task
-                    for (List<Object> timedtask : Database.listofaddedtasklist) {
-                        ArrayList<Object> timedtaskc = (ArrayList<Object>) timedtask;
-
-                        if (timedtaskc.contains(timedtaskid)) {
-                            int ind = Database.listofaddedtasklist.indexOf(timedtaskc);
-                            Timedtask.addtimedtasklist(timedtaskc, starDate, endDate);
-
-                            //************************Buna bir bakmam lazim **********************************************
-                            //listofaddedtasklist.remove(ind);
-
-
-                        }
-                    }
-                } else if (processid == 3) {
-
-                    for (List<Object> tlist : Database.listofaddedtasklist) {
-
-
-                        System.out.println("Task id:" + tlist.get(0) + "Task  details:" + tlist.get(1) + "Task Due Date:"
-                                + tlist.get(2) + "Task Status:" + tlist.get(3));
-                    }
-
-                } else if (processid == 4) {
-                    //ttlist is meaning timed task
-                    for (List<Object> ttlist : Database.listofaddedtimedtasklist) {
-
-                        System.out.println("Task id:" + ttlist.get(0) + "Task  details:" + ttlist.get(1) + "Task Due Date:"
-                                + ttlist.get(2) + "Task Status:" + ttlist.get(3) + "Task start date:" + ttlist.get(4) + "Task end date:"
-                                + ttlist.get(5));
-
-                    }
-                } else if (processid == 5) {
-
-                    System.out.println("which task do you want to assigned to task  please enter id");
-
-                    int assignedid = read.nextInt();
-
-                    System.out.println("Who do you want to assign?");
-                    String assignto = read.next();
-
-                    for (List<Object> astask : Database.listofaddedtasklist) {
-
-                        if (astask.contains(assignedid)) {
-
-                            Assignedtask.assignedtotask(astask, assignto);
-                        }
-                    }
-
-                } else if (processid == 6) {
-
-                    for (List<Object> aslist : Database.listofassignedtasklist) {
-
-                        System.out.println(" ASSIGNED TASK LIST " + "TASK ID:" + aslist.get(0) + "  TASK DETAILS:" + aslist.get(1) +
-                                "  TASK DUE DATE:" + aslist.get(2) + "TASK STATUS:" +
-                                aslist.get(3) + "   TASK ASSIGNED TO:" + aslist.get(4));
-                    }
-                } else if (processid == 7) {
-
-                    System.out.println("which task do you want to  bugget task");
-                    int id = read.nextInt();
-                    System.out.println("please add task startdate like this format  (yyyy-MM-dd)");
-                    String startdate = read.next();
-                    startdate += "T12:00:00";
-
-                    LocalDateTime starDate = LocalDateTime.parse(startdate);
-                    System.out.println("please add task finishdate like this format  (yyyy-MM-dd)");
-                    String finishdate = read.next();
-                    finishdate += "T12:00:00";
-
-                    LocalDateTime endDate = LocalDateTime.parse(finishdate);
-
-                    System.out.println("what isthe budget of  project");
-                    int budget = read.nextInt();
-
-                    for (List<Object> blist : Database.listofassignedtasklist) {
-                        if (blist.contains(id)) {
-                            AssignedTimedTask.tobudgetthetask(blist, starDate, endDate, budget);
-                        }
-
-                    }
-                } else if (processid == 8) {
-
-                    for (List<Object> bdlist : Database.listofbudgettasklist) {
-
-                        System.out.println(bdlist);
-
-                        System.out.println("Budget list" + "task id" + bdlist.get(0) + "task details:" + bdlist.get(1) +
-                                "task due date" + bdlist.get(2) + "task status" + bdlist.get(3) + "task assigned to" + bdlist.get(4) +
-                                "start date" + bdlist.get(5) + "end date" + bdlist.get(6) + "budget" + bdlist.get(7));
-                    }
-
-                } else if (processid == 9) {
-
-                    List<Object> result = Database.listofaddedtasklist.stream().sorted((o1, o2) -> o1.get(1).toString().compareTo(o2.get(1).toString())).collect(Collectors.toList());
-                    System.out.println("Tasks sorted  according to name :");
-
-                    for (Object task : result) {
-                        System.out.println(task);
-                    }
-
-                } else if (processid == 10) {
-
-                    System.out.println("Tasks sorted according to date");
-
-                    List<Object> result = Database.listofaddedtasklist.stream().sorted((o1, o2) -> o1.get(2).toString().compareTo(o2.get(2).toString())).collect(Collectors.toList());
-
-                    System.out.println(result);
-                } else if (processid == 11) {
+                else if (processid == 11) {
                     System.out.println("which task do you want to delete ");
                     int id = read.nextInt();
-
-
-                    for (List<Object> task : Database.listofaddedtasklist) {
-
-                        if (task.contains(id)) {
-
+                    for (Task task : Database.listofaddedtasklist) {
+                        if (task.getId()==id) {
                             Database.listofaddedtasklist.remove(task);
+                            break;
                         }
                     }
-
-
-                } else if (processid == 12) {
-
-
+                }
+                else if (processid == 12) {
                     System.out.println("which task's name do you want to update");
                     read.nextLine();
                     int id = Integer.valueOf(read.nextLine());
                     System.out.println("Please write what do you want to update");
                     String newname = read.nextLine();
-
-                    for (List<Object> task : Database.listofaddedtasklist) {
-                        if (task.contains(id)) {
-                            task.set(task.indexOf(task.get(1)), newname);
+                    for (Task task : Database.listofaddedtasklist) {
+                        if (task.getId()==id) {
+                            task.setName(newname);
                             System.out.println(id + " Task is updated.");
                             System.out.println(task);
+                            break;
                         }
                     }
-                } else if (processid == 13) {
+                }
+                else if (processid == 13) {
 
                     System.out.println("which task do you want to update due date  enter task id");
                     int id = read.nextInt();
@@ -290,15 +149,19 @@ public class Console {
 
                     LocalDateTime newduedaten = LocalDateTime.parse(newduedate);
 
-                    for (List<Object> task : Database.listofaddedtasklist) {
-                        if (task.contains(id)) {
+                    for (Task task : Database.listofaddedtasklist) {
+                        if (task.getId()==id) {
 
-                            task.set(2, newduedaten);//task.indexOf(task.get(2))
+                            task.setDueDate(newduedaten);
                             System.out.println("Last value of Duedate" + task);
+                            break;
 
                         }
                     }
-                } else if (processid == 14) {
+                }
+
+                /*
+                 else if (processid == 14) {
 
                     Jsonmake js = new Jsonmake();
 
